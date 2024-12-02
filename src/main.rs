6606f38a98bcf7cca194f3be916aad3000797ee0
Hugo Trippaers <hugo@trippaers.nl>
2024-12-02 22:04:25 +0100

    Configure the USART2 to send a message

--- src/main.rs
+++ src/main.rs
@@ -105,12 +105,129 @@ fn systemclock_config(dp: &pac::Peripherals) {
 
 }
 
+fn config_gpio(dp: &pac::Peripherals) {
+    // Enable Clock to GPIOA & GPIOC
+    dp.RCC
+        .ahb1enr()
+        .write(|w| w
+            .gpioaen().set_bit()
+            .gpiocen().set_bit()
+        );
+}
+
+fn config_usart2(dp: &pac::Peripherals) {
+    // Init the clock
+    dp.RCC
+        .apb1enr()
+        .write(|w| w
+            .usart2en().set_bit()
+        );
+    
+    dp.GPIOA
+        .moder()
+        .write(|w| unsafe { w
+            .moder2().bits(0b10)
+            .moder3().bits(0b10)
+        });
+
+    dp.GPIOA
+        .otyper()
+        .write(|w|  w
+            .ot2().clear_bit()
+            .ot3().clear_bit()
+        );
+
+    dp.GPIOA
+        .ospeedr()
+        .write(|w| unsafe { w
+            .ospeedr2().bits(0b10)
+            .ospeedr3().bits(0b10)
+        });
+
+    dp.GPIOA
+        .pupdr()
+        .write(|w| unsafe { w
+            .pupdr2().bits(0b00)
+            .pupdr3().bits(0b00)
+        });
+
+    dp.GPIOA
+        .afrl()
+        .write(|w| unsafe { w
+            .afrl2().bits(0b0111)
+            .afrl3().bits(0b0111)
+        });
+
+    // Disable USART2
+    dp.USART2
+        .cr1()
+        .write(|w| w.ue().clear_bit());
+
+    // 8N1 / oversampling 16 / transmit enabled
+    // Config CR2 Stopbits
+    dp.USART2
+        .cr2()
+        .modify(|_,w| unsafe {
+            w.stop().bits(0b00)
+        });
+
+    // Config CR1 Wordlength, Parity, Mode, Oversampling
+    dp.USART2
+        .cr1()
+        .modify(|_, w|
+            w.m().clear_bit()
+            .pce().clear_bit()
+            .over8().clear_bit()
+            .te().set_bit()
+        );
+
+    // Config CR3 disable CTSE, RTSE (hw flowcontrol)
+    dp.USART2
+        .cr3()
+        .modify(|_, w|
+            w.rtse().clear_bit()
+            .ctse().clear_bit()
+        );
+
+    // Determine BRR from PCLK1 frq (84Mhz DIV/2) and baudrate
+    let brr = baud_to_brr(84_000_000/2, 115_200);
+    dp.USART2
+        .brr()
+        .write(|w| unsafe { w.bits(brr) });
+
+    // Clear CR2 LINEN, CLKEN
+    dp.USART2
+        .cr2()
+        .modify(|_,w|
+            w.linen().clear_bit()
+            .clken().clear_bit()
+        );
+
+    // Clear CR3 SCEN, HDSEL, IREN
+    dp.USART2
+        .cr3()
+        .modify(|_,w| 
+            w.scen().clear_bit()
+            .hdsel().clear_bit()
+            .iren().clear_bit()
+        );
+
+    // Enable USART2
+    dp.USART2
+        .cr1()
+        .modify(|_, w| w.ue().set_bit());
+}
+
 #[entry]
 fn main() -> ! {
     // Setup handler for device peripherals
     let dp = pac::Peripherals::take().unwrap();
 
     systemclock_config(&dp);
+
+    config_gpio(&dp);
+
+    config_usart2(&dp);
 
     let mut cp = cortex_m::Peripherals::take().unwrap();
     unsafe {
@@ -119,17 +236,23 @@ fn main() -> ! {
         cp.SYST.csr.write(0x0105)
     };
 
-    // Enable Clock to GPIOA & GPIOC
-    dp.RCC
-        .ahb1enr()
-        .write(|w| w.gpioaen().set_bit().gpiocen().set_bit());
+    dp.GPIOA.moder().modify(|_, w| unsafe { w.moder5().bits(0b01) });
 
-    dp.GPIOA.moder().write(|w| unsafe { w.moder5().bits(0b01) });
-
+    let mut ctr = 0;
+    let mut idx = 0;
+    let message = "Hello World!\r\n";
     loop {
         if cp.SYST.has_wrapped() {
             dp.GPIOA.odr().modify(|r, w| w.odr5().bit(!r.odr5().bit()));
         }
+
+        if ctr == 0 {
+            let val = message.as_bytes()[idx];
+            idx = (idx + 1) % message.as_bytes().len();
+            dp.USART2
+                .dr().write(|w| unsafe { w.dr().bits(val as u16)});
+        }
+        ctr = (ctr + 1) % 100000;
 
         // Read PC13 Input Value
         // if !dp.GPIOC.idr().read().idr13().bit() {
@@ -140,4 +263,11 @@ fn main() -> ! {
         //     dp.GPIOA.odr().write(|w| w.odr5().clear_bit());
         // }
     }
+}
+
+fn baud_to_brr(fclk :u64, baudrate :u64) -> u32 {
+    let div16 = (fclk * 25) / (4 * baudrate);
+    let divmant =  div16 / 100;
+    let divfraq = (((div16 - (divmant * 100)) * 16) + 50) / 100;
+     ((divmant << 4) + (divfraq & 0xF0) + (divfraq & 0x0F)) as u32
 }
